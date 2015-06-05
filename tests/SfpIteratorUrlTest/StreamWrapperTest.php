e11c4ef5a86526a42d83b37bf4bbf0566aaf8ea5
sasezaki <sasezaki@gmail.com>
2015-06-06 01:48:53 +0900

    unit test - add get_contents

--- tests/SfpIteratorUrlTest/StreamWrapperTest.php
+++ tests/SfpIteratorUrlTest/StreamWrapperTest.php
@@ -2,8 +2,17 @@
 namespace SfpIteratorUrlTest;
 
 use PHPUnit_Framework_TestCase;
+use ArrayIterator;
+use SfpIteratorUrl\IteratorUrl;
 
 class StreamWrapperTest extends PHPUnit_Framework_TestCase
 {
-    
+    public function testRead()
+    {
+        $chrs = range('a', 'z');
+        $iterator = new ArrayIterator($chrs);
+        $fp = (new IteratorUrl())->open($iterator);
+        
+        $this->assertEquals(implode('', $chrs), stream_get_contents($fp, -1, 0));
+    }
 }
