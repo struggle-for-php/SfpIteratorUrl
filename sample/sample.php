c4344b475655d08a80666d55b7eb2d4f016ba793
sasezaki <sasezaki@gmail.com>
2015-06-06 01:39:39 +0900

    dev dependecies

--- sample/sample.php
+++ sample/sample.php
@@ -2,7 +2,7 @@
 
 use SfpIteratorUrl\IteratorUrl;
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 class Repeat extends IteratorIterator
 {
---
+++ tests/SfpIteratorUrlTest/StreamWrapperTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace SfpIteratorUrlTest;
+
+use PHPUnit_Framework_TestCase;
+
+class StreamWrapperTest extends PHPUnit_Framework_TestCase
+{
+    
+}
