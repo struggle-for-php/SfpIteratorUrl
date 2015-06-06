424c39710483b98f86cad24b1f4f429997b1f319
sasezaki <sasezaki@gmail.com>
2015-06-07 00:33:43 +0900

    strtolower for CONST , apply php-cs-fixer

--- src/SfpIteratorUrl/IteratorUrl.php
+++ src/SfpIteratorUrl/IteratorUrl.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace SfpIteratorUrl;
 
 use Iterator;
@@ -6,31 +7,32 @@ use Iterator;
 class IteratorUrl
 {
     protected $protocol;
-    
+
     public function __construct($protocol = 'SfpIteratorUrl')
     {
         $this->protocol = $protocol;
     }
-    
+
     protected function register()
     {
         if (!in_array($this->protocol, stream_get_wrappers())) {
             stream_wrapper_register($this->protocol, StreamWrapper::class);
         }
     }
-    
+
     public function open(Iterator $iterator)
     {
         $this->register();
         $iterator->rewind();
-        
+
         $ctx = stream_context_create([
             StreamWrapper::class => [
-                StreamWrapper::CONTEXT_KEY_ITERATOR => $iterator
-            ]
+                StreamWrapper::CONTEXT_KEY_ITERATOR => $iterator,
+            ],
         ]);
-        
+
         $fp = fopen($this->protocol.'://', 'r', false, $ctx);
+
         return $fp;
     }
 }
