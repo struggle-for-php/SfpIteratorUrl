4d0540e50343e6b1f35b586241d46ac04b235bab
sasezaki <sasezaki@gmail.com>
2015-11-14 13:15:49 +0900

    update sample

--- sample/sample_generator.php
+++ sample/sample_generator.php
@@ -4,26 +4,29 @@ use SfpIteratorUrl\IteratorUrl;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-class Repeat extends IteratorIterator
-{
-    public function current()
-    {
-        $str = str_repeat($this->getInnerIterator()->current(), 1024 * 1024);
-        return $str;
-    }
-}
+// よく見かける アクション内でのecho
+// ob_start
+// foreach (range('a', 'z') as $char) {
+//     echo str_repeat($char, 1024 * 1024);
+//     ob_flush();
+// }
 
-$iterator = new Repeat(new ArrayIterator(range('a', 'z')));
+$gen = function () {    
+    foreach (range('a', 'z') as $char) {
+        yield str_repeat($char, 1024 * 1024);
+    }
+};
+
+
+$iterator = $gen();
 
 $fp = (new IteratorUrl)->open($iterator);
 
 $body = new Zend\Diactoros\Stream($fp);
-
-//fpassthru($body->detach()); // use 3.49 MB
-//echo $body->__toString();   // use 28.49MB
-
+fpassthru($body->detach());
+echo $body->__toString();                          
 echo "\n";
-echo "memory used ",(formatBytes(memory_get_peak_usage()));
+echo "memory used ",(formatBytes(memory_get_peak_usage()));  
 
 // http://qiita.com/suin/items/0090ab167bbdb3d77181
 function formatBytes($bytes, $precision = 2, array $units = null)
